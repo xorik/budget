37302ae3c73241c10e6c28dd6d9e50e880500261
xorik <xor29a@gmail.com>
2020-02-16 12:18:15 +0100

    Replace default icon

---
+++ front/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png',
+    },
+
+    themeColor: '#555555',
+    appleMobileWebAppCapable: 'yes',
+  },
+
+  productionSourceMap: false,
+}
